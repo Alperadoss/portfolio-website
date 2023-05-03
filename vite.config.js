0f3f8fc0371237631b72f3956479bac0220e7cea
Alper Yazagan <alperyazagan@gmail.com>
2023-05-03 11:20:04 +0300

    deploy added

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
-// vite.config.js
-export default {
-  // config options
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   plugins: [react()],
++  base: '/vite-deploy-demo/'
+})
